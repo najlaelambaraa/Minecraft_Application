750350a2170149d680c8275ab68aca0c9eaf1890
Augustin <augustin.affognon@etu.uca.fr>
2023-02-28 23:14:16 +0100

    Correction

--- Sources/BlazorT/Services/IInventoryDataService.cs
+++ Sources/BlazorT/Services/IInventoryDataService.cs
@@ -5,18 +5,50 @@ namespace BlazorT.Services
 {
     public interface IInventoryDataService
     {
-        Task Add(ItemModel model);
+        /// <summary>
+        /// Ajoute un élément à l'inventaire.
+        /// </summary>
+        /// <param name="model">Modèle de l'élément à ajouter.</param>
+        public   Task Add(ItemModel model);
 
-        Task<int> Count();
+        /// <summary>
+        /// Récupère le nombre total d'éléments dans l'inventaire.
+        /// </summary>
+        /// <returns>Le nombre total d'éléments dans l'inventaire.</returns>
+        public   Task<int> Count();
 
-        Task<List<Item>> List(int currentPage, int pageSize);
+        /// <summary>
+        /// Récupère une liste d'éléments de l'inventaire.
+        /// </summary>
+        /// <param name="currentPage">La page courante de la liste.</param>
+        /// <param name="pageSize">Le nombre d'éléments par page.</param>
+        /// <returns>Une liste d'éléments de l'inventaire.</returns>
+        public   Task<List<Item>> List(int currentPage, int pageSize);
 
-        Task<Item> GetById(int id);
+        /// <summary>
+        /// Récupère un élément de l'inventaire par ID.
+        /// </summary>
+        /// <param name="id">ID de l'élément à récupérer.</param>
+        /// <returns>L'élément de l'inventaire correspondant à l'ID spécifié.</returns>
+        public   Task<Item> GetById(int id);
 
-        Task Update(int id, ItemModel model);
+        /// <summary>
+        /// Met à jour un élément de l'inventaire.
+        /// </summary>
+        /// <param name="id">ID de l'élément à mettre à jour.</param>
+        /// <param name="model">Modèle de l'élément à mettre à jour.</param>
+        public   Task Update(int id, ItemModel model);
 
-        Task Delete(int id);
+        /// <summary>
+        /// Supprime un élément de l'inventaire.
+        /// </summary>
+        /// <param name="id">ID de l'élément à supprimer.</param>
+        public   Task Delete(int id);
 
-        Task<List<InventoryRecipe>> GetRecipes();
+        /// <summary>
+        /// Récupère une liste de recettes d'artisanat.
+        /// </summary>
+        /// <returns>Une liste de recettes d'artisanat.</returns>
+        public   Task<List<InventoryRecipe>> GetRecipes();
     }
 }
